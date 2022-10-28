931d2d9f5a7a75718e3154c782ebc818e932bb0e
Basudev Bharat Bhushan <bharatbhushan173@gmail.com>
2022-10-29 00:52:40 +0530

    Proxy Added, uri updated

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -10,7 +10,7 @@ import {
 import { setContext } from "apollo-link-context"; //npm i apollo-link-context
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:5000/graphql",
+  uri: "https://social-media-web-server.onrender.com/",
 });
 
 const authLink = setContext((_, { headers }) => {
